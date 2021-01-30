df0d5c7298a8b414b5c852aa752c0c5ab0e66dbf
actions_roldan <jdroldan@playvox.com>
2021-01-29 19:38:13 -0500

    configurations settings

--- elenasTest/core/settings.py
+++ elenasTest/core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'elenasTest.urls'
+ROOT_URLCONF = 'elenasTest.core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'elenasTest.wsgi.application'
+WSGI_APPLICATION = 'elenasTest.core.wsgi.application'
 
 
 # Database
